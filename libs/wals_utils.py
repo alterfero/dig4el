784a67dd3728e36d1d2b2ed6a0399784d270724f
Alterfero <alterfero@gmail.com>
2024-07-22 08:20:47 -1000

    wals integration, language monography

--- libs/wals_utils.py
+++ libs/wals_utils.py
@@ -2,6 +2,63 @@ import os
 import json
 from libs import utils as u
 
+# parameter.csv list parameters by pk and their names
+#
+# domainelement.csv lists all the existing values of these parameters, referencing parameter pk
+#
+# value.csv and valueset.csv tell what languages uses which value by referencing the value id, the language pk and the parameter pk
+# They share the same pk.
+#
+# language.csv contains all the languages with their pk, id, name and location
+
+def get_language_data_by_id(language_id):
+    with open("./external_data/wals_derived/language_by_pk_lookup_table.json") as f:
+        language_by_pk_lookup_table = json.load(f)
+    with open("./external_data/wals_derived/valueset_by_pk_lookup_table.json") as f:
+        valueset_by_pk_lookup_table = json.load(f)
+    with open("./external_data/wals_derived/domain_element_by_pk_lookup_table.json") as f:
+        domainelement_by_pk_lookup_table = json.load(f)
+    with open("./external_data/wals_derived/parameter_pk_by_name_lookup_table.json") as f:
+        parameter_pk_by_name = json.load(f)
+    values = u.csv_to_dict("./external_data/wals-master/raw/value.csv")
+    for pk in language_by_pk_lookup_table.keys():
+        if language_by_pk_lookup_table[pk]["id"] == language_id:
+            selected_language_pk = pk
+            print("selected language pk: {}".format(selected_language_pk))
+            break
+    valueset_list = []
+    vpks = []
+    # retrieving all value_pk associated with this language_pk
+    for pk in valueset_by_pk_lookup_table:
+        if str(valueset_by_pk_lookup_table[pk]["language_pk"]) == selected_language_pk:
+            vpks.append(pk)
+    #print("vpks: ",vpks)
+    for item in values:
+        if str(item["valueset_pk"]) in vpks:
+            valueset_list.append({"valueset_pk":item["valueset_pk"], "domainelement_pk":item["domainelement_pk"]})
+    # now getting an organized dict with parameter and parameter_value for that language
+    result_dict = {}
+    for item in valueset_list:
+        parameter_pk = domainelement_by_pk_lookup_table[str(item["domainelement_pk"])]["parameter_pk"]
+        for name in parameter_pk_by_name:
+            if str(parameter_pk_by_name[name]) == str(parameter_pk):
+                parameter_name = name
+                break
+            else:
+                print("no match for parameter pk {}".format(parameter_pk))
+                parameter_name = None
+        domainelement_name = domainelement_by_pk_lookup_table[str(item["domainelement_pk"])]["name"]
+        result_dict[parameter_pk] = {
+            "parameter": parameter_name,
+            "value": domainelement_name,
+            "domainelement_pk": item["domainelement_pk"],
+            "valueset_pk": item["valueset_pk"]
+        }
+
+    return result_dict
+
+
+
 def build_parameter_pk_by_name_lookup_table():
     print("build_parameter_pk_by_name_lookup_table")
     parameter = u.csv_to_dict("./external_data/wals-master/raw/parameter.csv")
@@ -21,8 +78,9 @@ def load_parameter_pk_by_name_lookup_table():
         print("domain_elements_pk_by_parameter_pk_lookup_table not found in the file system, building it.")
         return build_parameter_pk_by_name_lookup_table()
 
-def build_domain_elements_pk_by_parameter_pk_lookup_table(domain_element):
+def build_domain_elements_pk_by_parameter_pk_lookup_table():
     print("build_domain_element_by_parameter_pk_lookup_table")
+    domain_element = u.csv_to_dict("./external_data/wals-master/raw/domainelement.csv")
     domain_elements_pk_by_parameter_pk_lookup_table = {}
     for entry in domain_element:
         if entry["parameter_pk"] not in domain_elements_pk_by_parameter_pk_lookup_table:
@@ -34,16 +92,17 @@ def build_domain_elements_pk_by_parameter_pk_lookup_table(domain_element):
         json.dump(domain_elements_pk_by_parameter_pk_lookup_table, f)
     return domain_elements_pk_by_parameter_pk_lookup_table
 
-def load_domain_elements_pk_by_parameter_pk_lookup_table(domain_element):
+def load_domain_elements_pk_by_parameter_pk_lookup_table():
     if "domain_elements_pk_by_parameter_pk_lookup_table.json" in os.listdir("./external_data/wals_derived/"):
         with open("./external_data/wals_derived/domain_elements_pk_by_parameter_pk_lookup_table.json") as f:
             return json.load(f)
     else:
         print("domain_elements_pk_by_parameter_pk_lookup_table not found in the file system, building it.")
-        return build_domain_elements_pk_by_parameter_pk_lookup_table(domain_element)
+        return build_domain_elements_pk_by_parameter_pk_lookup_table()
 
-def build_domain_element_by_pk_lookup_table(domain_element):
+def build_domain_element_by_pk_lookup_table():
     print("build_domain_element_by_pk_lookup_table")
+    domain_element = u.csv_to_dict("./external_data/wals-master/raw/domainelement.csv")
     domain_element_by_pk_lookup_table = {}
     for entry in domain_element:
         if entry["pk"] not in domain_element_by_pk_lookup_table:
@@ -55,16 +114,17 @@ def build_domain_element_by_pk_lookup_table(domain_element):
         json.dump(domain_element_by_pk_lookup_table, f)
     return domain_element_by_pk_lookup_table
 
-def load_domain_element_by_pk_lookup_table(domain_element):
+def load_domain_element_by_pk_lookup_table():
     if "domain_element_by_pk_lookup_table.json" in os.listdir("./external_data/wals_derived/"):
         with open ("./external_data/wals_derived/domain_element_by_pk_lookup_table.json") as f:
             return json.load(f)
     else:
         print("domain_element_by_pk_lookup_table not found in the file system, building it.")
-        return build_domain_element_by_pk_lookup_table(domain_element)
+        return build_domain_element_by_pk_lookup_table()
 
-def build_value_by_domain_element_pk_lookup_table(values):
+def build_value_by_domain_element_pk_lookup_table():
     print("build_value_by_domain_element_pk_lookup_table")
+    values = u.csv_to_dict("./external_data/wals-master/raw/value.csv")
     value_by_domain_element_pk_lookup_table = {}
     for value in values:
         if value["domainelement_pk"] not in value_by_domain_element_pk_lookup_table:
@@ -76,16 +136,17 @@ def build_value_by_domain_element_pk_lookup_table(values):
         json.dump(value_by_domain_element_pk_lookup_table, f)
     return value_by_domain_element_pk_lookup_table
 
-def load_value_by_domain_element_pk_lookup_table(values):
+def load_value_by_domain_element_pk_lookup_table():
     if "value_by_domain_element_pk_lookup_table.json" in os.listdir("./external_data/wals_derived/"):
         with open("./external_data/wals_derived/value_by_domain_element_pk_lookup_table.json", "r") as f:
             return json.load(f)
     else:
         print("build_value_by_domain_element_pk_lookup_table not found in the file system, building it.")
-        return build_value_by_domain_element_pk_lookup_table(values)
+        return build_value_by_domain_element_pk_lookup_table()
 
-def build_valueset_by_pk_lookup_table(valueset):
+def build_valueset_by_pk_lookup_table():
     print("build_valueset_by_pk_lookup_table")
+    valueset = u.csv_to_dict("./external_data/wals-master/raw/valueset.csv")
     valueset_by_pk_lookup_table = {}
     for v in valueset:
         valueset_by_pk_lookup_table[v["pk"]] = v
@@ -94,16 +155,17 @@ def build_valueset_by_pk_lookup_table(valueset):
         json.dump(valueset_by_pk_lookup_table, f)
     return valueset_by_pk_lookup_table
 
-def load_valueset_by_pk_lookup_table(valueset):
+def load_valueset_by_pk_lookup_table():
     if "valueset_by_pk_lookup_table.json" in os.listdir("./external_data/wals_derived/"):
         with open("./external_data/wals_derived/valueset_by_pk_lookup_table.json", "r") as f:
             return json.load(f)
     else:
         print("valueset_by_pk_lookup_table not found in the file system, building it.")
-        return build_valueset_by_pk_lookup_table(valueset)
+        return build_valueset_by_pk_lookup_table()
 
-def build_language_by_pk_lookup_table(language):
+def build_language_by_pk_lookup_table():
     print("build_language_by_pk_lookup_table")
+    language = u.csv_to_dict("./external_data/wals-master/raw/language.csv")
     language_by_pk_lookup_table = {}
     for l in language:
         language_by_pk_lookup_table[l["pk"]] = l
@@ -112,13 +174,13 @@ def build_language_by_pk_lookup_table(language):
         json.dump(language_by_pk_lookup_table, f)
     return language_by_pk_lookup_table
 
-def load_language_by_pk_lookup_table(language):
+def load_language_by_pk_lookup_table():
     if "language_by_pk_lookup_table.json" in os.listdir("./external_data/wals_derived/"):
         with open("./external_data/wals_derived/language_by_pk_lookup_table.json", "r") as f:
             return json.load(f)
     else:
         print("language_by_pk_lookup_table not found in the file system, building it.")
-        return build_language_by_pk_lookup_table(language)
+        return build_language_by_pk_lookup_table()
 
 def build_language_info_by_id_lookup_table():
     print("build_language_info_by_id_lookup_table")
--- pages/8_WALS_Access.py
+++ pages/8_WALS_Access.py
@@ -3,6 +3,7 @@ import pandas as pd
 from libs import utils as u, wals_utils as wu
 import os
 import json
+from streamlit_agraph import agraph, Node, Edge, Config
 
 wals_data_folder = "./external_data/wals-master/raw/"
 
@@ -23,48 +24,29 @@ if "parameters" not in st.session_state:
     st.session_state["parameters"] = parameters
     print("{} parameters: {}".format(len(parameters), parameters[10]))
 
-if "domainelement" not in st.session_state:
-    domainelement = u.csv_to_dict(wals_data_folder + "domainelement.csv")
-    st.session_state["domainelement"] = domainelement
-    print("{} domainelement: {}".format(len(domainelement), domainelement[10]))
-
-if "values" not in st.session_state:
-    values = u.csv_to_dict(wals_data_folder + "value.csv")
-    st.session_state["values"] = values
-    print("{} values: {}".format(len(values), values[10]))
-
-if "valueset" not in st.session_state:
-    valueset = u.csv_to_dict(wals_data_folder + "valueset.csv")
-    st.session_state["valueset"] = valueset
-    print("{} valueset: {}".format(len(valueset), valueset[10]))
-
-if "languages" not in st.session_state:
-    languages = u.csv_to_dict(wals_data_folder + "language.csv")
-    st.session_state["languages"] = languages
-    print("{} languages: {}".format(len(languages), languages[10]))
 
 if "parameter_pk_by_name_lookup_table" not in st.session_state:
     st.session_state["parameter_pk_by_name_lookup_table"] = wu.load_parameter_pk_by_name_lookup_table()
     print("parameter_pk_by_name_lookup_table loaded")
 
 if "value_by_domain_element_pk_lookup_table" not in st.session_state:
-    st.session_state["value_by_domain_element_pk_lookup_table"] = wu.load_value_by_domain_element_pk_lookup_table(st.session_state["values"])
+    st.session_state["value_by_domain_element_pk_lookup_table"] = wu.load_value_by_domain_element_pk_lookup_table()
     print("value_by_domain_element_pk_lookup_table loaded")
 
 if "valueset_by_pk_lookup_table" not in st.session_state:
-    st.session_state["valueset_by_pk_lookup_table"] = wu.load_valueset_by_pk_lookup_table(st.session_state["valueset"])
+    st.session_state["valueset_by_pk_lookup_table"] = wu.load_valueset_by_pk_lookup_table()
     print("valueset_by_pk_lookup_table loaded")
 
 if "language_by_pk_lookup_table" not in st.session_state:
-    st.session_state["language_by_pk_lookup_table"] = wu.load_language_by_pk_lookup_table(st.session_state["languages"])
+    st.session_state["language_by_pk_lookup_table"] = wu.load_language_by_pk_lookup_table()
     print("language_by_pk_lookup_table loaded")
 
 if "domain_elements_pk_by_parameter_pk_lookup_table" not in st.session_state:
-    st.session_state["domain_elements_pk_by_parameter_pk_lookup_table"] = wu.build_domain_elements_pk_by_parameter_pk_lookup_table(st.session_state["domainelement"])
+    st.session_state["domain_elements_pk_by_parameter_pk_lookup_table"] = wu.build_domain_elements_pk_by_parameter_pk_lookup_table()
     print("domain_elements_pk_by_parameter_pk_lookup_table loaded")
 
 if "domain_elements_by_pk_lookup_table" not in st.session_state:
-    st.session_state["domain_elements_by_pk_lookup_table"] = wu.load_domain_element_by_pk_lookup_table(st.session_state["domainelement"])
+    st.session_state["domain_elements_by_pk_lookup_table"] = wu.load_domain_element_by_pk_lookup_table()
     print("domain_elements_by_pk_lookup_table loaded")
 
 if "language_info_by_id_lookup_table" not in st.session_state:
@@ -76,6 +58,21 @@ with st.popover("Credits"):
     st.markdown("Dryer, Matthew S. & Haspelmath, Martin (eds.) 2013. The World Atlas of Language Structures Online. Leipzig: Max Planck Institute for Evolutionary Anthropology. (Available online at https://wals.info)")
     st.markdown("Dataset version 2020.3, https://doi.org/10.5281/zenodo.7385533")
     st.markdown("Dataset under Creative Commons licence CC BY 4.0 https://creativecommons.org/licenses/by/4.0/")
+
+with st.expander("Language monography"):
+    language_id_by_name = {}
+    for language_id in st.session_state["language_info_by_id_lookup_table"].keys():
+        language_info = st.session_state["language_info_by_id_lookup_table"][language_id]
+        language_id_by_name[language_info["name"]] = language_id
+    selected_language_name = st.selectbox("languages", list(language_id_by_name.keys()))
+    selected_language_id = language_id_by_name[selected_language_name]
+    # retrieving language_pk
+    result_dict = wu.get_language_data_by_id(selected_language_id)
+    st.write("{} parameters with a value for {}.".format(len(result_dict), selected_language_name))
+    result_df = pd.DataFrame(result_dict).T
+    st.dataframe(result_df)
+
+
 
 with st.expander("Exploration by language and parameter"):
     language_id_by_name = {}
@@ -113,7 +110,6 @@ with st.expander("Exploration by language and parameter"):
         colw.write("Subfamily: {}".format(language_info["subfamily"]))
         colw.write("Genus: {}".format(language_info["genus"]))
 
-
     selected_language_ids = []
     for language_name in selected_language_names:
         selected_language_ids.append(language_id_by_name[language_name])
@@ -132,27 +128,97 @@ with st.expander("Exploration by language and parameter"):
             if k not in selected_language_ids:
                 selected_language_ids.append(k)
     st.write("{} languages selected".format(len(selected_language_ids)))
+    current_languages = []
+    for l in selected_language_ids:
+        current_languages.append(st.session_state["language_info_by_id_lookup_table"][l]["name"])
 
     param_pk = st.session_state["parameter_pk_by_name_lookup_table"][selected_param]
     selected_param_domain_element_pks = st.session_state["domain_elements_pk_by_parameter_pk_lookup_table"][param_pk]
     result_dict = {}
     # for each domain element, access all existing languages in the selected languages that match this value
     for de_pk in selected_param_domain_element_pks:
-        de_name = st.session_state["domain_elements_by_pk_lookup_table"][str(de_pk)]["name"]
-        result_dict[de_name] = []
-        values_pk = [v["pk"] for v in st.session_state["value_by_domain_element_pk_lookup_table"][str(de_pk)]]
-        for value_pk in values_pk:
-            language_pk = st.session_state["valueset_by_pk_lookup_table"][str(value_pk)]["language_pk"]
-            language_id = st.session_state["language_by_pk_lookup_table"][str(language_pk)]["id"]
-            print(language_pk, language_id)
-            if language_id in selected_language_ids:
-                result_dict[de_name].append(language_id)
+        if str(de_pk) in st.session_state["domain_elements_by_pk_lookup_table"]:
+            de_name = st.session_state["domain_elements_by_pk_lookup_table"][str(de_pk)]["name"]
+            result_dict[de_name] = []
+            values_pk = [v["pk"] for v in st.session_state["value_by_domain_element_pk_lookup_table"][str(de_pk)]]
+            for value_pk in values_pk:
+                language_pk = st.session_state["valueset_by_pk_lookup_table"][str(value_pk)]["language_pk"]
+                language_id = st.session_state["language_by_pk_lookup_table"][str(language_pk)]["id"]
+                if language_id in selected_language_ids:
+                    result_dict[de_name].append(language_id)
+        else:
+            print("no {} in domain_elements_by_pk_lookup_table".format(de_pk))
     stats_dict = {}
     for k in result_dict.keys():
         stats_dict[k] = len(result_dict[k])
+
+    if len(result_dict) != 0:
+        nodes = []
+        edges = []
+        #print(selected_param)
+        nodes.append(Node(id=selected_param,
+                          label=selected_param,
+                          size=50,
+                          color="brown",
+                          title=selected_param,
+                          symbolType="circle"))
+        for v in result_dict:
+            #print(v)
+            nodes.append(Node(id=v,
+                              label=v,
+                              size=30,
+                              color="green",
+                              title=v,
+                              symbolType="circle"))
+            edges.append(Edge(source=selected_param,
+                              physics=True,
+                              target=v,
+                              type="SMOOTH",
+                              color="brown"))
+            c = 0
+            for l in result_dict[v]:
+                c += 1
+                if c<300:
+                    nodes.append(Node(id=l,
+                                      label=st.session_state["language_info_by_id_lookup_table"][l]["name"],
+                                      size=15,
+                                      color="blue",
+                                      title=l,
+                                      symbolType="circle"))
+                    edges.append(Edge(source=v,
+                                      physics=True,
+                                      target=l,
+                                      type="SMOOTH",
+                                      color="brown"))
+        # ---- graph
+        config = Config(width=800,
+                        height=600,
+                        directed=True,
+                        physics=True,
+                        dashed=True,
+                        hierarchical=False,
+                        nodeHighlightBehavior=True,
+                        highlightColor="#F7A7A6",  # or "blue"
+                        collapsible=True,
+                        automaticRearrangeAfterDropNode=False,
+                        node={'labelProperty': 'label'},
+                        link={'labelProperty': 'label', 'renderLabel': True}
+                        # **kwargs e.g. node_size=1000 or node_color="blue"
+                        )
+        return_value = agraph(nodes=nodes,
+                              edges=edges,
+                              config=config)
+
     stats_df = pd.DataFrame(stats_dict, index=["number of languages"]).T
     st.bar_chart(stats_df, y_label="values of the parameter", x_label="number of languages",
                  horizontal=True)
+    if st.checkbox("List languages"):
+        for v in result_dict:
+            st.subheader(v)
+            for l in result_dict[v]:
+                info = st.session_state["language_info_by_id_lookup_table"][l]
+                st.write("***{}***, family: {}, genus: {}, macroarea: {}".format(info["name"], info["family"], info["genus"], info["macroarea"]))
+
 
 with st.expander("Exploration by parameter and value"):
     #for parameter in parameters:
@@ -216,11 +282,6 @@ with st.expander("Exploration by parameter and value"):
                 }
     languages_df = pd.DataFrame.from_dict(selected_language_dict).T
     st.dataframe(languages_df)
-
-
-
-
-
 
 
 
