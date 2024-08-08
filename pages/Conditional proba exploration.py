1f79463edea22478296d2651285bf7117ead1938
Alterfero <alterfero@gmail.com>
2024-08-08 11:31:28 -1000

    expander added

--- pages/Conditional proba exploration.py
+++ pages/Conditional proba exploration.py
@@ -98,77 +98,77 @@ with st.expander("Explore conditional probabilities associated with the values o
         st.write("Columns are the values of the chosen parameter.")
         st.write("Rows are the values from other parameters that are the most conditioned by the occurrence of the value of the chosen parameter. When there is no name for the value, the identifier is used")
 
-st.write("Explore conditional probabilities deriving from a combination of observations")
-st.write("Enter up to 5 grammatical observations made on a language below to see a list of the most probable values associated with these observations")
+with st.expander("Explore joint conditional probabilities deriving from a combination of observations"):
+    st.write("Enter below grammatical observations made on a language to see a list of the most probable other values associated with these observations")
 
-if st.button("Reset observations"):
-    st.session_state["obs"] = []
-    st.session_state["obs_de_pk_list"] = []
-    st.session_state["current_cpt"] = st.session_state["cpt"]
-if len(st.session_state["obs"]) < 6:
-    new_pm_obs = st.selectbox("parameter "+str(len(st.session_state["obs"]) + 1), parameter_name_list_filtered, key="obs_pm"+str(len(st.session_state["obs"])))
-    obs_pm_pk = st.session_state["parameter_pk_by_name"][str(new_pm_obs)]
-    avail_values_pk = st.session_state["domain_elements_pk_by_parameter_pk"][str(obs_pm_pk)]
-    avail_values_names = []
-    for pk in avail_values_pk:
-        avail_values_names.append(st.session_state["domain_element_by_pk"][str(pk)]["name"])
-    obs_de_name = st.selectbox("select the observed value of that paramater", avail_values_names, key="obs_value"+str(len(st.session_state["obs"])))
-    obs_de_index = avail_values_names.index(obs_de_name)
-    obs_de_pk = avail_values_pk[obs_de_index]
-    if st.button("add observation"):
-        st.session_state["obs"].append({"param_pk":obs_pm_pk, "de_pk":obs_de_pk, "param_name":new_pm_obs, "de_name":obs_de_name})
-        st.session_state["obs_de_pk_list"].append(obs_de_pk)
+    if st.button("Reset observations"):
+        st.session_state["obs"] = []
+        st.session_state["obs_de_pk_list"] = []
         st.session_state["current_cpt"] = st.session_state["cpt"]
-if st.session_state["obs"] != []:
-    obsc = 0
-    st.write("Current observations:")
-    for item in st.session_state["obs"]:
-        obsc += 1
-        st.write("{}. {}: {}".format(obsc, item["param_name"], item["de_name"]))
+    if len(st.session_state["obs"]) < 6:
+        new_pm_obs = st.selectbox("parameter "+str(len(st.session_state["obs"]) + 1), parameter_name_list_filtered, key="obs_pm"+str(len(st.session_state["obs"])))
+        obs_pm_pk = st.session_state["parameter_pk_by_name"][str(new_pm_obs)]
+        avail_values_pk = st.session_state["domain_elements_pk_by_parameter_pk"][str(obs_pm_pk)]
+        avail_values_names = []
+        for pk in avail_values_pk:
+            avail_values_names.append(st.session_state["domain_element_by_pk"][str(pk)]["name"])
+        obs_de_name = st.selectbox("select the observed value of that paramater", avail_values_names, key="obs_value"+str(len(st.session_state["obs"])))
+        obs_de_index = avail_values_names.index(obs_de_name)
+        obs_de_pk = avail_values_pk[obs_de_index]
+        if st.button("add observation"):
+            st.session_state["obs"].append({"param_pk":obs_pm_pk, "de_pk":obs_de_pk, "param_name":new_pm_obs, "de_name":obs_de_name})
+            st.session_state["obs_de_pk_list"].append(obs_de_pk)
+            st.session_state["current_cpt"] = st.session_state["cpt"]
+    if st.session_state["obs"] != []:
+        obsc = 0
+        st.write("Current observations:")
+        for item in st.session_state["obs"]:
+            obsc += 1
+            st.write("{}. {}: {}".format(obsc, item["param_name"], item["de_name"]))
 
-    # transform the cpt table to compute the joint conditional probability
+        # transform the cpt table to compute the joint conditional probability
 
-    #remove columns that correspond to observed events
-    st.session_state["current_cpt"] = st.session_state["cpt"].drop(columns=st.session_state["obs_de_pk_list"])
+        #remove columns that correspond to observed events
+        st.session_state["current_cpt"] = st.session_state["cpt"].drop(columns=st.session_state["obs_de_pk_list"])
 
-    # Sum the probabilities for the observed events
-    prob_sum_df = st.session_state["current_cpt"].loc[st.session_state["obs_de_pk_list"]].sum()
-    # Normalize the summed probabilities
-    normalized_prob_df = prob_sum_df / prob_sum_df.sum()
+        # Sum the probabilities for the observed events
+        prob_sum_df = st.session_state["current_cpt"].loc[st.session_state["obs_de_pk_list"]].sum()
+        # Normalize the summed probabilities
+        normalized_prob_df = prob_sum_df / prob_sum_df.sum()
 
 
-    #renaming rows
-    new_i_label = {}
-    for k in normalized_prob_df.index:
-        if str(k) in st.session_state["parameter_pk_by_domain_element_pk"].keys():
-            pm_pk = st.session_state["parameter_pk_by_domain_element_pk"][str(k)]
-            if str(pm_pk) in st.session_state["parameter_name_by_pk"].keys():
-                pm_name = str(st.session_state["parameter_name_by_pk"][str(pm_pk)])
+        #renaming rows
+        new_i_label = {}
+        for k in normalized_prob_df.index:
+            if str(k) in st.session_state["parameter_pk_by_domain_element_pk"].keys():
+                pm_pk = st.session_state["parameter_pk_by_domain_element_pk"][str(k)]
+                if str(pm_pk) in st.session_state["parameter_name_by_pk"].keys():
+                    pm_name = str(st.session_state["parameter_name_by_pk"][str(pm_pk)])
+                else:
+                    pm_name = "param name unknown"
             else:
-                pm_name = "param name unknown"
-        else:
-            pm_name = "param pk unknown"
-        if str(st.session_state["domain_element_by_pk"][str(k)]["name"]) != "nan":
-            new_label = pm_name + ": " + str(st.session_state["domain_element_by_pk"][str(k)]["name"])
-        elif str(st.session_state["domain_element_by_pk"][str(k)]["description"]) != "nan":
-            new_label = pm_name + ": " + str(st.session_state["domain_element_by_pk"][str(k)]["description"])
-        else:
-            new_label = pm_name + ": " + "pk " + str(k)
-        new_i_label[k] = new_label
+                pm_name = "param pk unknown"
+            if str(st.session_state["domain_element_by_pk"][str(k)]["name"]) != "nan":
+                new_label = pm_name + ": " + str(st.session_state["domain_element_by_pk"][str(k)]["name"])
+            elif str(st.session_state["domain_element_by_pk"][str(k)]["description"]) != "nan":
+                new_label = pm_name + ": " + str(st.session_state["domain_element_by_pk"][str(k)]["description"])
+            else:
+                new_label = pm_name + ": " + "pk " + str(k)
+            new_i_label[k] = new_label
 
 
-    normalized_prob_df = normalized_prob_df.rename(index=new_i_label)
+        normalized_prob_df = normalized_prob_df.rename(index=new_i_label)
 
-    colq, colw = st.columns(2)
-    percentile = colq.slider("probability percentile threshold", min_value=0.0, max_value=1.0, step=0.01, value=0.9)
-    percentile_threshold = normalized_prob_df.quantile(percentile)
-    colw.write("Probability threshold of percentile {}: {}".format(percentile, round(percentile_threshold,5)))
+        colq, colw = st.columns(2)
+        percentile = colq.slider("probability percentile threshold", min_value=0.0, max_value=1.0, step=0.01, value=0.9)
+        percentile_threshold = normalized_prob_df.quantile(percentile)
+        colw.write("Probability threshold of percentile {}: {}".format(percentile, round(percentile_threshold,5)))
 
-    normalized_prob_df = normalized_prob_df[normalized_prob_df >= percentile_threshold]
+        normalized_prob_df = normalized_prob_df[normalized_prob_df >= percentile_threshold]
 
-    normalized_prob_df = normalized_prob_df.sort_values(ascending=False)[:100]
+        normalized_prob_df = normalized_prob_df.sort_values(ascending=False)[:100]
 
-    st.dataframe(normalized_prob_df, use_container_width=True)
+        st.dataframe(normalized_prob_df, use_container_width=True)
 
 
 
