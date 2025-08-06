7a9d7e19bfa6521d6d0c8929e0bc426650e52750
Alterfero <alterfero@gmail.com>
2025-08-06 12:41:25 -1000

    railway volume path and file viewer in railway_checks.py

--- pages/railway_checks.py
+++ pages/railway_checks.py
@@ -3,10 +3,25 @@
 import os
 import streamlit as st
 
+base_dir = os.environ.get("RAILWAY_VOLUME_MOUNT_PATH")
+
+if "path" not in st.session_state:
+    st.session_state.path = [base_dir, "storage"]
+
 
 st.write("CWD: {}".format(os.getcwd()))
 st.write("Volume mount: {}".format(os.environ.get("RAILWAY_VOLUME_MOUNT_PATH")))
-st.write("Listing: {}".format(os.listdir(os.environ.get("RAILWAY_VOLUME_MOUNT_PATH"))))
-for directory in os.listdir(os.environ.get("RAILWAY_VOLUME_MOUNT_PATH")):
-    st.write(directory)
-    st.write(os.listdir(os.path.join(os.environ.get("RAILWAY_VOLUME_MOUNT_PATH"), directory)))
+
+st.write("Current path: {}".format(st.session_state.path))
+
+for item in os.listdir(st.session_state.current_dir):
+    if st.button(item, key=item):
+        st.session_state.path.append(item)
+
+
+st.write(os.path.join(*st.session_state.path))
+
+
+if st.button("reset"):
+    st.session_state.path = ["storage"]
+    st.rerun()
---
+++ script/flex_viewer.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import xml.etree.ElementTree as ET
+import pandas as pd
+
+
+def parse_flex_xml(xml_content):
+    root = ET.fromstring(xml_content)
+
+    data = []
+
+    for phrase in root.findall(".//phrase"):
+        sentence_urn = phrase.find("item[@type='txt'][@lang='urn']").text
+        sentence_num = phrase.find("item[@type='segnum'][@lang='en']").text
+
+        words_data = []
+        for word in phrase.findall("words/word"):
+            txt_elem = word.find("item[@type='txt'][@lang='urn']")
+            punct_elem = word.find("item[@type='punct'][@lang='urn']")
+
+            if punct_elem is not None:
+                words_data.append({"Word": punct_elem.text, "Gloss": "", "Morphemes": []})
+                continue
+
+            word_text = txt_elem.text if txt_elem is not None else ""
+            gloss_elem = word.find("item[@type='gls'][@lang='en']")
+            word_gloss = gloss_elem.text if gloss_elem is not None else ""
+
+            morphemes_data = []
+            for morph in word.findall("morphemes/morph"):
+                morph_text = morph.find("item[@type='txt'][@lang='urn']")
+                morph_gloss = morph.find("item[@type='gls'][@lang='en']")
+
+                morphemes_data.append({
+                    "Morph": morph_text.text if morph_text is not None else "",
+                    "Gloss": morph_gloss.text if morph_gloss is not None else ""
+                })
+
+            words_data.append({"Word": word_text, "Gloss": word_gloss, "Morphemes": morphemes_data})
+
+        data.append({
+            "Sentence Number": sentence_num,
+            "Sentence (URN)": sentence_urn,
+            "Words": words_data
+        })
+
+    return data
+
+
+st.title("FLEX XML Linguistics Visualizer")
+
+uploaded_file = st.file_uploader("Upload XML file", type="xml")
+
+if uploaded_file:
+    xml_content = uploaded_file.read().decode("utf-8")
+    parsed_data = parse_flex_xml(xml_content)
+
+    for sentence in parsed_data:
+        st.subheader(f"Sentence {sentence['Sentence Number']}")
+        st.write(sentence["Sentence (URN)"])
+
+        for word in sentence["Words"]:
+            st.markdown(f"**{word['Word']}** - *{word['Gloss']}*")
+
+            if word['Morphemes']:
+                morph_df = pd.DataFrame(word['Morphemes'])
+                st.table(morph_df)
