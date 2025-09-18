9c7dd3074e740bf67cc8515f6fc03b1f5db63401
Alterfero <alterfero@gmail.com>
2025-09-18 07:56:59 +0200

    New overview video, directly accessible.

--- home.py
+++ home.py
@@ -28,97 +28,101 @@ st.markdown("""*DIG4EL (Digital Inferential Grammar for Endangered Languages) is
             DIG4EL follows the [FAIR](https://www.go-fair.org/fair-principles/) and 
             [CARE](https://www.gida-global.org/care) principles for data governance and respect of their owners.*""")
 
+h1, h2 = st.columns(2)
+with h1:
+    with st.popover("Notice", width='stretch'):
+        st.markdown(""" 
+        *Notice: DIG4EL is currently in the early production stage.*
+    1) **Public nature of outputs**: DIG4EL outputs may incorporate content from all data created or uploaded 
+    about a language. The dissemination of these outputs is beyond the control and knowledge of the DIG4EL team. 
+    The DIG4EL team assumes no responsibility or liability for the use, interpretation, or redistribution of 
+    these outputs. 
+    2) **Expert review requirement**: System outputs are provided for expert review and editing. All outputs are 
+    available in editable formats for this purpose.
+    3) **Limitation of liability**: The DIG4EL team disclaims any responsibility for the use, modification, 
+    or distribution of system outputs by third parties.
+    4) **Access to resource-intensive features**: Certain computationally intensive features are restricted. 
+    Researchers working on the description or teaching of endangered languages may request access by contacting 
+    the DIG4EL team.
+        """)
 
-with st.popover("Notice", width='stretch'):
-    st.markdown(""" 
-    *Notice: DIG4EL is currently in the early production stage.*
-1) **Public nature of outputs**: DIG4EL outputs may incorporate content from all data created or uploaded 
-about a language. The dissemination of these outputs is beyond the control and knowledge of the DIG4EL team. 
-The DIG4EL team assumes no responsibility or liability for the use, interpretation, or redistribution of 
-these outputs. 
-2) **Expert review requirement**: System outputs are provided for expert review and editing. All outputs are 
-available in editable formats for this purpose.
-3) **Limitation of liability**: The DIG4EL team disclaims any responsibility for the use, modification, 
-or distribution of system outputs by third parties.
-4) **Access to resource-intensive features**: Certain computationally intensive features are restricted. 
-Researchers working on the description or teaching of endangered languages may request access by contacting 
-the DIG4EL team.
-    """)
+    with st.popover("Guests, Members and Caretakers", width='stretch'):
+        st.markdown("""
+        - You can explore DIG4EL without logging in, as a **Guest**. Guests have access only to a restricted 
+        set of languages and cannot upload or augment data. However, they can generate content within 
+        the restricted collection.
+        - **Members** can log in to access additional features. **If you are working on a language or teaching it**, 
+        contact us to become a Member!
+        - Each language has designated **Caretakers**. Caretakers are Members with full upload 
+        and augmentation rights for one or more languages. They can add, remove, and edit all the information 
+        DIG4EL holds about the languages they manage. Contact us if you would like to become a Caretaker!
+        """)
 
-with st.popover("Guests, Members and Caretakers", width='stretch'):
-    st.markdown("""
-    - You can explore DIG4EL without logging in, as a **Guest**. Guests have access only to a restricted 
-    set of languages and cannot upload or augment data. However, they can generate content within 
-    the restricted collection.
-    - **Members** can log in to access additional features. **If you are working on a language or teaching it**, 
-    contact us to become a Member!
-    - Each language has designated **Caretakers**. Caretakers are Members with full upload 
-    and augmentation rights for one or more languages. They can add, remove, and edit all the information 
-    DIG4EL holds about the languages they manage. Contact us if you would like to become a Caretaker!
-    """)
+    with st.popover("Getting started", width='stretch'):
+        st.subheader("Getting started")
+        get1, get2 = st.columns(2)
+        with get1:
+            st.markdown("5-minute overview video")
+            st.video("https://youtu.be/EiogHTmpiHc")
+        with get2:
+            st.markdown("""Click on **Dashboard** on the left menu to create and upload information about a language 
+            or click on **Generate Grammar** to use available data to generate grammatical descriptions."
+                        """)
 
-with st.popover("Getting started", width='stretch'):
-    st.subheader("Getting started")
-    get1, get2 = st.columns(2)
-    with get1:
-        st.markdown("8-minute overview video")
-        st.video("https://youtu.be/K0J48jiflwE")
-    with get2:
-        st.markdown("""Click on **Dashboard** on the left menu to create and upload information about a language 
-        or click on **Generate Grammar** to use available data to generate grammatical descriptions."
-                    """)
+    with st.popover("References", width='stretch'):
 
-with st.popover("References", width='stretch'):
+        st.markdown("**Acknowledgement**")
+        st.markdown("We want to express here our profound gratitude to all community members from all over the world who generously shared their time, knowledge, languages, and insights, to make this work possible. ")
 
-    st.markdown("**Acknowledgement**")
-    st.markdown("We want to express here our profound gratitude to all community members from all over the world who generously shared their time, knowledge, languages, and insights, to make this work possible. ")
+        st.divider()
 
-    st.divider()
+        st.markdown("""
+        **How to Cite DIG4EL:** 
+    
+        To cite DIG4EL in your research, please reference the software and the original paper:
+        - Software: 
+        CHRISTIAN, S. (2025). DIG4EL (v1.0.1). Zenodo. https://doi.org/10.5281/zenodo.16944459
+        - Paper:
+        Sebastien Christian,
+        Enhancing grammatical documentation for endangered languages with graph-based meaning representation and Loopy Belief Propagation,
+        Natural Language Processing Journal,
+        Volume 12,
+        2025,
+        100164,
+        ISSN 2949-7191,
+        https://doi.org/10.1016/j.nlp.2025.100164.
+        (https://www.sciencedirect.com/science/article/pii/S2949719125000408)
+    
+        """)
 
-    st.markdown("""
-    **How to Cite DIG4EL:** 
+        st.divider()
 
-    To cite DIG4EL in your research, please reference the software and the original paper:
-    - Software: 
-    CHRISTIAN, S. (2025). DIG4EL (v1.0.1). Zenodo. https://doi.org/10.5281/zenodo.16944459
-    - Paper:
-    Sebastien Christian,
-    Enhancing grammatical documentation for endangered languages with graph-based meaning representation and Loopy Belief Propagation,
-    Natural Language Processing Journal,
-    Volume 12,
-    2025,
-    100164,
-    ISSN 2949-7191,
-    https://doi.org/10.1016/j.nlp.2025.100164.
-    (https://www.sciencedirect.com/science/article/pii/S2949719125000408)
+        st.markdown("**World Atlas of Language Structures**")
+        st.markdown(
+            "Dryer, Matthew S. & Haspelmath, Martin (eds.) 2013. The World Atlas of Language Structures Online. Leipzig: Max Planck Institute for Evolutionary Anthropology.")
+        st.markdown("Dataset version 2020.3, https://doi.org/10.5281/zenodo.7385533")
+        st.markdown("Dataset under Creative Commons licence CC BY 4.0 https://creativecommons.org/licenses/by/4.0/")
 
-    """)
+        st.divider()
 
-    st.divider()
+        st.markdown("**Grambank**")
+        st.markdown("""
+        Skirgård, Hedvig, Hannah J. Haynie, Harald Hammarström, Damián E. Blasi, Jeremy Collins, Jay Latarche, Jakob Lesage, Tobias Weber, Alena Witzlack-Makarevich, Michael Dunn, Ger Reesink, Ruth Singer, Claire Bowern, Patience Epps, Jane Hill, Outi Vesakoski, Noor Karolin Abbas, Sunny Ananth, Daniel Auer, Nancy A. Bakker, Giulia Barbos, Anina Bolls, Robert D. Borges, Mitchell Browen, Lennart Chevallier, Swintha Danielsen, Sinoël Dohlen, Luise Dorenbusch, Ella Dorn, Marie Duhamel, Farah El Haj Ali, John Elliott, Giada Falcone, Anna-Maria Fehn, Jana Fischer, Yustinus Ghanggo Ate, Hannah Gibson, Hans-Philipp Göbel, Jemima A. Goodall, Victoria Gruner, Andrew Harvey, Rebekah Hayes, Leonard Heer, Roberto E. Herrera Miranda, Nataliia Hübler, Biu H. Huntington-Rainey, Guglielmo Inglese, Jessica K. Ivani, Marilen Johns, Erika Just, Ivan Kapitonov, Eri Kashima, Carolina Kipf, Janina V. Klingenberg, Nikita König, Aikaterina Koti, Richard G. A. Kowalik, Olga Krasnoukhova, Kate Lynn Lindsey, Nora L. M. Lindvall, Mandy Lorenzen, Hannah Lutzenberger, Alexandra Marley, Tânia R. A. Martins, Celia Mata German, Suzanne van der Meer, Jaime Montoya, Michael Müller, Saliha Muradoglu, HunterGatherer, David Nash, Kelsey Neely, Johanna Nickel, Miina Norvik, Bruno Olsson, Cheryl Akinyi Oluoch, David Osgarby, Jesse Peacock, India O.C. Pearey, Naomi Peck, Jana Peter, Stephanie Petit, Sören Pieper, Mariana Poblete, Daniel Prestipino, Linda Raabe, Amna Raja, Janis Reimringer, Sydney C. Rey, Julia Rizaew, Eloisa Ruppert, Kim K. Salmon, Jill Sammet, Rhiannon Schembri, Lars Schlabbach, Frederick W. P. Schmidt, Dineke Schokkin, Jeff Siegel, Amalia Skilton, Hilário de Sousa, Kristin Sverredal, Daniel Valle, Javier Vera, Judith Voß, Daniel Wikalier Smith, Tim Witte, Henry Wu, Stephanie Yam, Jingting Ye 葉婧婷, Maisie Yong, Tessa Yuditha, Roberto Zariquiey, Robert Forkel, Nicholas Evans, Stephen C. Levinson, Martin Haspelmath, Simon J. Greenhill, Quentin D. Atkinson & Russell D. Gray (2023). Grambank v1.0 (v1.0) [Data set]. Zenodo. https://doi.org/10.5281/zenodo.7740140
+        """)
 
-    st.markdown("**World Atlas of Language Structures**")
-    st.markdown(
-        "Dryer, Matthew S. & Haspelmath, Martin (eds.) 2013. The World Atlas of Language Structures Online. Leipzig: Max Planck Institute for Evolutionary Anthropology.")
-    st.markdown("Dataset version 2020.3, https://doi.org/10.5281/zenodo.7385533")
-    st.markdown("Dataset under Creative Commons licence CC BY 4.0 https://creativecommons.org/licenses/by/4.0/")
+        st.divider()
 
-    st.divider()
+        st.markdown("**Conversational Questionnaires**")
+        st.markdown("François, A. 2019. A proposal for conversational questionnaires In Lahaussois A., Vuillermet M.Methodological Tools for Linguistic Description and Typology, 16, , pp.155-196, 2019, Language Documentation & Conservation Special Publications, 978-0-9973295-5-1. ffhal-02061237f. https://hal.science/hal-02061237/document")
+        st.link_button("Read the paper online", "https://hal.science/hal-02061237/document")
 
-    st.markdown("**Grambank**")
-    st.markdown("""
-    Skirgård, Hedvig, Hannah J. Haynie, Harald Hammarström, Damián E. Blasi, Jeremy Collins, Jay Latarche, Jakob Lesage, Tobias Weber, Alena Witzlack-Makarevich, Michael Dunn, Ger Reesink, Ruth Singer, Claire Bowern, Patience Epps, Jane Hill, Outi Vesakoski, Noor Karolin Abbas, Sunny Ananth, Daniel Auer, Nancy A. Bakker, Giulia Barbos, Anina Bolls, Robert D. Borges, Mitchell Browen, Lennart Chevallier, Swintha Danielsen, Sinoël Dohlen, Luise Dorenbusch, Ella Dorn, Marie Duhamel, Farah El Haj Ali, John Elliott, Giada Falcone, Anna-Maria Fehn, Jana Fischer, Yustinus Ghanggo Ate, Hannah Gibson, Hans-Philipp Göbel, Jemima A. Goodall, Victoria Gruner, Andrew Harvey, Rebekah Hayes, Leonard Heer, Roberto E. Herrera Miranda, Nataliia Hübler, Biu H. Huntington-Rainey, Guglielmo Inglese, Jessica K. Ivani, Marilen Johns, Erika Just, Ivan Kapitonov, Eri Kashima, Carolina Kipf, Janina V. Klingenberg, Nikita König, Aikaterina Koti, Richard G. A. Kowalik, Olga Krasnoukhova, Kate Lynn Lindsey, Nora L. M. Lindvall, Mandy Lorenzen, Hannah Lutzenberger, Alexandra Marley, Tânia R. A. Martins, Celia Mata German, Suzanne van der Meer, Jaime Montoya, Michael Müller, Saliha Muradoglu, HunterGatherer, David Nash, Kelsey Neely, Johanna Nickel, Miina Norvik, Bruno Olsson, Cheryl Akinyi Oluoch, David Osgarby, Jesse Peacock, India O.C. Pearey, Naomi Peck, Jana Peter, Stephanie Petit, Sören Pieper, Mariana Poblete, Daniel Prestipino, Linda Raabe, Amna Raja, Janis Reimringer, Sydney C. Rey, Julia Rizaew, Eloisa Ruppert, Kim K. Salmon, Jill Sammet, Rhiannon Schembri, Lars Schlabbach, Frederick W. P. Schmidt, Dineke Schokkin, Jeff Siegel, Amalia Skilton, Hilário de Sousa, Kristin Sverredal, Daniel Valle, Javier Vera, Judith Voß, Daniel Wikalier Smith, Tim Witte, Henry Wu, Stephanie Yam, Jingting Ye 葉婧婷, Maisie Yong, Tessa Yuditha, Roberto Zariquiey, Robert Forkel, Nicholas Evans, Stephen C. Levinson, Martin Haspelmath, Simon J. Greenhill, Quentin D. Atkinson & Russell D. Gray (2023). Grambank v1.0 (v1.0) [Data set]. Zenodo. https://doi.org/10.5281/zenodo.7740140
-    """)
+        st.divider()
 
-    st.divider()
+        st.markdown("**Didactic grammars of endangered languages**")
+        st.markdown("Vernaudon J. 2013. L’enseignement des langues kanak en Nouvelle-Calédonie. Hermès. n° 65. , [ p.]. 10.4267/2042/51507 or Vernaudon, Jacques. (2018). Les métalangues du tahitien à l'école, https://www.researchgate.net/publication/333261526_Les_metalangues_du_tahitien_a_l'ecole")
 
-    st.markdown("**Conversational Questionnaires**")
-    st.markdown("François, A. 2019. A proposal for conversational questionnaires In Lahaussois A., Vuillermet M.Methodological Tools for Linguistic Description and Typology, 16, , pp.155-196, 2019, Language Documentation & Conservation Special Publications, 978-0-9973295-5-1. ffhal-02061237f. https://hal.science/hal-02061237/document")
-    st.link_button("Read the paper online", "https://hal.science/hal-02061237/document")
-
-    st.divider()
-
-    st.markdown("**Didactic grammars of endangered languages**")
-    st.markdown("Vernaudon J. 2013. L’enseignement des langues kanak en Nouvelle-Calédonie. Hermès. n° 65. , [ p.]. 10.4267/2042/51507 or Vernaudon, Jacques. (2018). Les métalangues du tahitien à l'école, https://www.researchgate.net/publication/333261526_Les_metalangues_du_tahitien_a_l'ecole")
+with h2:
+    st.video("https://youtu.be/EiogHTmpiHc")
 
 st.markdown("For any enquiry, contact sebastien.christian@upf.pf")
 st.markdown("----------------------------------------------------")
